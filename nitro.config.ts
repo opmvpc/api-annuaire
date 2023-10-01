96428c7b5164a027b180ee90a18c9085478cce61
opmvpc <thibsix@outlook.be>
2023-10-01 17:36:08 +0200

    1 file modified

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,6 +1,12 @@
 export default defineNitroConfig({
   routeRules: {
-    '*': {
+    '/img/**': {
+      cors: true,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+    },
+    '/api/**': {
       cors: true,
       headers: {
         'Access-Control-Allow-Origin': '*',
