7a31e1703b8efbe73fa2116fd79a22aadf621eeb
opmvpc <thibsix@outlook.be>
2023-10-01 18:08:35 +0200

    1 file modified

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,1 +1,9 @@
-export default defineNitroConfig({});
+export default defineNitroConfig({
+  publicAssets: [
+    {
+      baseURL: 'img',
+      dir: 'public/img',
+      maxAge: 60 * 60 * 24 * 7,
+    },
+  ],
+});
